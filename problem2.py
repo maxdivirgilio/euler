16d6b9297910e8f5307e6ddd2d2d631f79eb9d38
Maxim DiVirgilio <msd14@scasd.org>
2016-01-24 15:34:56 -0500

    Final Code problem2

--- problem2.py
+++ problem2.py
@@ -1,16 +1,10 @@
-mySum=0
+mySum=2
 x = 1
 y = 2
-if x < 4000000:
-    and y < 4000000:
+while x < 4000000 and y < 4000000:
     z = x + y
-    if z%2==0:
-        mySum = mySum + z
-while x < 4000000:
-    and y < 4000000:
     x = y
     y = z
-    z = x + y
-if z%2==0:
-    mySum = mySum + z
+    if z%2==0:
+        mySum = mySum + z
 print(mySum)
